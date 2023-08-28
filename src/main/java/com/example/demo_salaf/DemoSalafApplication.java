82e0782a7df9bd1c75cb983c4d2ec3e363655ac7
JhonatasMOURA <99987649+JhonatasMOURA@users.noreply.github.com>
2023-08-28 22:50:00 +0000

    novo

--- src/main/java/com/example/demo_salaf/DemoSalafApplication.java
+++ src/main/java/com/example/demo_salaf/DemoSalafApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DemoSalafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSalafApplication.class, args);
 	}
 
+	@GetMapping("hello")
+	public String hello(){
+		return "Germinare tech - Nikinho";
+	}
 }
